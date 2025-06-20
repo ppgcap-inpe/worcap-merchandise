90ac957bd3608433277538cb18f9bdc946a6a41e
PPG-CAP <cap.inpe.discentes@gmail.com>
2025-06-19 21:59:30 -0300

    dataProducts.js

--- dataProducts.js
+++ dataProducts.js
@@ -48,7 +48,7 @@ const productsData = [
   {
     id: 6,
     name: "Copo Cordel",
-    photo: "https://www.meucopoeco.com.br/customizer/?id_produto=59&medida=2&ferramenta=1749693678850.png",
+    photo: "https://drive.google.com/file/d/1Q8XnYfMADnL2feJQUO_zFUW1EBxyOWMd",
     price: 15.00,
     category: "copo",
     description: "Copo de 500ml com temática cordel."
